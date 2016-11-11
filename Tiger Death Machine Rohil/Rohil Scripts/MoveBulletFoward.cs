953036f43f5876420cc476fb9afcde814f7edc7a
Rohil Aniruth <anirr579@newschool.edu>
2016-11-11 15:34:10 -0500

    Rohil's Scripts
    Basic player movement, enemy movement, figuring out goddamn projectiles.

---
+++ Tiger Death Machine Rohil/Rohil Scripts/MoveBulletFoward.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBulletFoward : MonoBehaviour {
+
+    float maxSpeed = 5f;
+
+	void Update () {
+
+        Vector3 pos = transform.position;
+
+        Vector3 velocity = new Vector3(0, maxSpeed * Time.deltaTime, 0);
+
+        pos += transform.rotation * velocity;
+
+        transform.position = pos; 
+	
+	}
+}
