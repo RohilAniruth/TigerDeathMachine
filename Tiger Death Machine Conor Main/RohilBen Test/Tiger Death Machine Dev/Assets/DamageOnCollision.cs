30427a5b76768805a61a74927f69222236cab06e
Conor Behrens <cdbehrens96@gmail.com>
2016-11-12 08:43:41 -0500

    Added Pew

---
+++ Tiger Death Machine Conor Main/RohilBen Test/Tiger Death Machine Dev/Assets/DamageOnCollision.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOnCollision : MonoBehaviour
+{
+
+    GameObject scoreUITextGO;
+
+    
+
+
+
+    int GameScore = 1;
+
+    void Start()
+    {
+        scoreUITextGO = GameObject.FindGameObjectWithTag("ScoreHolder");
+
+    }
+
+
+        void OnTriggerEnter2D()
+    {
+        Debug.Log("Trigger!");
+
+        GameScore--;
+        if (GameScore >= 1000)
+        {
+
+            Destroy(gameObject);
+
+
+
+        }
+
+
+
+        scoreUITextGO.GetComponent<ScoreTracker>().addScore(10);
+
+    }
+
+
+
+
+
+   void Die()
+    {
+        Destroy(gameObject);
+   }
+}
